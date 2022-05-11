29e180731a0e13c4d8a1e5d50e846f5794025850
tehforsch <mail@tonipeter.de>
2022-05-11 22:32:13 +0200

    adjust ports to avoid a potentially pre-existing port

--- kobo/remote/run.sh
+++ kobo/remote/run.sh
@@ -2,6 +2,5 @@
 resolutionX=1024
 resolutionY=758
 while true; do
-    nc -l -p 6002 | xargs -n1 sh /remote/readLine.sh "$resolutionX" "$resolutionY"
-    sleep 1
+    nc -l -p 6302 | xargs -n1 sh /remote/readLine.sh "$resolutionX" "$resolutionY"
 done
--- kobo/remote/supershell/supershell.sh
+++ kobo/remote/supershell/supershell.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ip="192.168.178.20"
-nc -l -p 6003 | xargs -n1 /mnt/onboard/remote/supershell/readLine.sh 2>&1 | nc $ip 6004
+ip="192.168.0.123"
+nc -l -p 6303 | xargs -n1 /mnt/onboard/remote/supershell/readLine.sh 2>&1 | nc $ip 6304
