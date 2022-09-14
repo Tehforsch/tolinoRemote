7f3724a65a994c9b1dcd725aaf4151edfe011587
tehforsch <mail@tonipeter.de>
2022-09-14 23:24:23 +0200

    fix #6

--- kobo/remote/run.sh
+++ kobo/remote/run.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-resolutionX=1024
-resolutionY=758
+# In order to keep the TCP connection from the ESP32 open
+# we need to keep stdin open, since otherwise the netcat
+# version of the Kobo will send a "FIN ACK" signal, which
+# implies that it doesn't want to send anymore. The WifiClient
+# of the ESP32 will then close the connection (as opposed to
+# keeping the connection open only for sending to the Kobo)
+# This is different from how the OpenBSD version netcat behaves.
+# 
+# To do so, we need to pipe some process into netcat, but
+# this process also needs to die as soon as the netcat process dies.
+# Now, unfortunately we don't have process substitution here (bash
+# is not available) and we don't understand the shell well enough
+# to do this properly, so the "solution" is to not pipe directly into
+# netcat, but into a script which runs netcat and will kill all sleep
+# commands once netcat finishes. This is ugly as hell, but seems to work.
 while true; do
-    nc -l -p 6302 | xargs -n1 sh /remote/readLine.sh "$resolutionX" "$resolutionY"
+    sleep infinity | /remote/runNetCatAndKillSleep.sh | xargs -n1 sh /remote/readLine.sh 1024 758
 done
---
+++ kobo/remote/runNetCatAndKillSleep.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+nc -l -p 6302
+killall sleep
