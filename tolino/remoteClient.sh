eaffd2047cbb771a1c0a37e82cbe837945d30da4
tehforsch <mail@tonipeter.de>
2023-06-20 15:18:27 +0200

    fix port in tolino script

--- tolino/remoteClient.sh
+++ tolino/remoteClient.sh
@@ -3,6 +3,6 @@
 resolutionX=$(wm size | busybox tr -s " " | busybox cut -d " " -f 3 | busybox cut -d "x" -f 1)
 resolutionY=$(wm size | busybox tr -s " " | busybox cut -d " " -f 3 | busybox cut -d "x" -f 2)
 while [[ 1 ]]; do
-    /system/usr/remoteControl/netcat -d -l 5000 | busybox xargs -n1 sh /system/usr/remoteControl/readLine.sh "$resolutionX" "$resolutionY"
+    /system/usr/remoteControl/netcat -d -l 6302 | busybox xargs -n1 sh /system/usr/remoteControl/readLine.sh "$resolutionX" "$resolutionY"
     sleep 1
 done
